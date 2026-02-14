e09f4411ca629a041d90ef32d20df9f061f0e3f6
ChemieAi <kzlyburak@gmail.com>
2026-02-14 23:50:23 +0300

    detect click on plank and calculate distance from pivot

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Seesaw initialized");
+const plank = document.getElementById("plank");
+
+plank.addEventListener("click", (event) => {
+  const rect = plank.getBoundingClientRect();
+
+  // Click'in plank üzerindeki X pozisyonu
+  const clickX = event.clientX - rect.left;
+
+  // Pivot merkezde : plank genişliğinin yarısı
+  const pivotX = rect.width / 2;
+
+  // Distance: sola negatif, sağa pozitif
+  const distanceFromPivot = clickX - pivotX;
+
+  console.log("Click distance from pivot:", distanceFromPivot);
+});

851e81c4527b0db1c52aa3933d593413ae35d1a6
ChemieAi <kzlyburak@gmail.com>
2026-02-15 00:04:07 +0300

    calculate torque and rotate seesaw based on balance

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ plank.addEventListener("click", (event) => {
   objects.push(newObject);
 
   renderObject(newObject);
+  updateSeesaw();
   console.log(objects);
 });
 
@@ -35,4 +36,28 @@ function renderObject(obj) {
   el.textContent = obj.weight;
 
   plank.appendChild(el);
+}
+
+function updateSeesaw() {
+  let leftTorque = 0;
+  let rightTorque = 0;
+
+  objects.forEach((obj) => {
+    const torque = obj.weight * Math.abs(obj.distance);
+
+    if (obj.distance < 0) {
+      leftTorque += torque;
+    } else {
+      rightTorque += torque;
+    }
+  });
+
+  // Açı hesabı (+-30 sınır)
+  const angle = clamp((rightTorque - leftTorque) / 10, -30, 30);
+
+  plank.style.transform = `translateX(-50%) rotate(${angle}deg)`;
+}
+
+function clamp(value, min, max) {
+  return Math.max(min, Math.min(max, value));
 }
